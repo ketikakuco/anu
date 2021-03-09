2f71c59a7b248541649c2370caf2cee5ade19e8f
Your Name <you@example.com>
2021-03-09 14:29:46 +0000

    fill bytes

--- test.sh
+++ test.sh
@@ -3,14 +3,14 @@ echo "spam " >> "repo1/spamfile.txt"
 git pull
 git add .
 git commit -m "fill bytes"
-git push --force origin -all
+git push --force origin --all
 
 touch "repo1/spamfile.txt"
 echo "spam " >> "repo1/spamfile.txt"
 git pull
 git add .
 git commit -m "fill bytes"
-git push --force origin -all
+git push --force origin --all
 
 printf "akan di lanjutkan 5 jam 30 menit"
 sleep 19800
