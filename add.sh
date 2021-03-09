76e93e8d34f94d3c8ace55a3480abaec680600fb
Your Name <you@example.com>
2021-03-09 14:23:38 +0000

    fill bytes

---
+++ add.sh
@@ -0,0 +1,12 @@
+git remote rm origin
+read -p "Enter your github repository address: " GHADDRESS
+echo "Address: $GHADDRESS."
+
+env git remote add origin $GHADDRESS
+
+read -p "Enter your repository branch (main/master): " kntl
+echo "Branch: $kntl."
+env git branch -M $kntl
+
+printf "DONE"
+sleep 2
---
+++ test.sh
@@ -0,0 +1,24 @@
+touch "repo1/spamfile.txt"
+echo "spam " >> "repo1/spamfile.txt"
+git pull
+git add .
+git commit -m "fill bytes"
+git push --force origin main
+
+touch "repo1/spamfile.txt"
+echo "spam " >> "repo1/spamfile.txt"
+git pull
+git add .
+git commit -m "fill bytes"
+git push --force origin main
+
+touch "repo1/spamfile.txt"
+echo "spam " >> "repo1/spamfile.txt"
+git pull
+git add .
+git commit -m "fill bytes"
+git push --force origin main
+
+printf "akan di lanjutkan 23 menit"
+sleep 1380
+./test.sh
