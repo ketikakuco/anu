2211dcb5cb499af45ff91663b9ad2b30f808e6e6
Your Name <you@example.com>
2021-03-09 14:28:52 +0000

    fill bytes

--- test1.sh
+++ test1.sh
@@ -12,13 +12,7 @@ git add .
 git commit -m "fill bytes"
 git push --force origin main
 
-touch "repo1/spamfile.txt"
-echo "spam " >> "repo1/spamfile.txt"
-git pull
-git add .
-git commit -m "fill bytes"
-git push --force origin main
 
 printf "akan di lanjutkan 5 jam 30 menit"
-sleep 19800
-./test1.sh
+sleep 20000
+./test.sh
