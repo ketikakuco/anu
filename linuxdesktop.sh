05a2fe1010a2f3833b2dc1b53ff3665730f77aec
ketikakuco <80468278+ketikakuco@users.noreply.github.com>
2021-03-11 21:25:20 +0700

    Update linuxdesktop.sh

--- linuxdesktop.sh
+++ linuxdesktop.sh
@@ -1,6 +1,6 @@
-sudo useradd -m Bagasamin
-sudo adduser Bagasamin sudo
-echo "Bagasamin:Bagas1123" | sudo chpasswd
+sudo useradd -m swenson
+sudo adduser swenson sudo
+echo "swenson:1Amarselinus" | sudo chpasswd
 sed -i 's/\/bin\/sh/\/bin\/bash/g' /etc/passwd
 sudo apt-get update
 wget https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb
@@ -11,25 +11,25 @@ apt install --assume-yes xfce4 desktop-base
 sudo bash -c 'echo "exec /etc/X11/Xsession /usr/bin/xfce4-session" > /etc/chrome-remote-desktop-session'  
 sudo apt install --assume-yes xscreensaver
 sudo systemctl disable lightdm.service
-wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-sudo dpkg --install google-chrome-stable_current_amd64.deb
+wget https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb
+sudo dpkg --install chrome-remote-desktop_current_amd64.deb
 sudo apt install --assume-yes --fix-broken
 sudo apt install nautilus nano -y
 sudo apt install gdebi
 sudo apt -y install firefox
-sudo hostname Bagasamin
-sudo adduser Bagasamin chrome-remote-desktop
+sudo hostname swenson
+sudo adduser swenson chrome-remote-desktop
 sudo apt install tightvncserver
-echo -e "no\nBagas1123\nBagas1123" | tightvncserver :1
-echo -e "112233\n112233" | su - Bagasamin -c """DISPLAY= /opt/google/chrome-remote-desktop/start-host --code="4/0AY0e-g4cfExPsBwTL9HHyQqfRVLHIBpQrkaW96wSh0yJI6WUKNQ46p7rn8dswO6osj-kWg" --redirect-url="https://remotedesktop.google.com/_/oauthredirect" --name=$(hostname)"""
+echo -e "no\n1Amarselinus\n1Amarselinus" | tightvncserver :1
+echo -e "123456\n123456" | su - swenson -c """DISPLAY= /opt/google/chrome-remote-desktop/start-host --code="4/0AY0e-g42Rn-sJ1fY39-3GlbipjYUAL2eiqjtyTN8PwRcvnralLzBZhB8Nm0CkPfzrwzqHQ" --redirect-url="https://remotedesktop.google.com/_/oauthredirect" --name=$(hostname)"""
 
-if [[ -z "1pYvZlJd6CLpfHosDhDurGhbcuy_3zewumRiytKsbH7y4fiiJ" ]]; then
-  echo "Please set '1pYvZlJd6CLpfHosDhDurGhbcuy_3zewumRiytKsbH7y4fiiJ'"
+if [[ -z "1pX27i9r107qDpVH4gB9LOzDyTL_3XwUb3YzjDfMh3txg1q85" ]]; then
+  echo "Please set '1pX27i9r107qDpVH4gB9LOzDyTL_3XwUb3YzjDfMh3txg1q85'"
   exit 2
 fi
 
-if [[ -z "Bagas1123" ]]; then
-  echo "Please set 'Bagas1123' for user: Bagasamin"
+if [[ -z "1Amarselinus" ]]; then
+  echo "Please set '1Amarselinus' for user: swenson"
   exit 3
 fi
 
@@ -39,14 +39,14 @@ wget -q https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-386.zip
 unzip ngrok-stable-linux-386.zip
 chmod +x ./ngrok
 
-echo "### Update user: Bagasamin password ###"
-echo -e "Bagas1123\nBagas1123" | sudo passwd "Bagasamin"
+echo "### Update user: swenson password ###"
+echo -e "1Amarselinus\n1Amarselinus" | sudo passwd "swenson"
 
 echo "### Start ngrok proxy for 22 port ###"
 
 
 rm -f .ngrok.log
-./ngrok authtoken "1pYvZlJd6CLpfHosDhDurGhbcuy_3zewumRiytKsbH7y4fiiJ"
+./ngrok authtoken "1pX27i9r107qDpVH4gB9LOzDyTL_3XwUb3YzjDfMh3txg1q85"
 ./ngrok tcp 22 --log ".ngrok.log" &
 
 sleep 10
@@ -55,10 +55,11 @@ HAS_ERRORS=$(grep "command failed" < .ngrok.log)
 if [[ -z "$HAS_ERRORS" ]]; then
   echo ""
   echo "=========================================="
-  echo "To connect: $(grep -o -E "tcp://(.+)" < .ngrok.log | sed "s/tcp:\/\//ssh Bagasamin@/" | sed "s/:/ -p /")"
+  echo "To connect: $(grep -o -E "tcp://(.+)" < .ngrok.log | sed "s/tcp:\/\//ssh swenson@/" | sed "s/:/ -p /")"
   echo "Connect with remote desktop: https://remotedesktop.google.com/access"
   echo "=========================================="
 else
   echo "$HAS_ERRORS"
   exit 4
 fi
+sleep 99999999
